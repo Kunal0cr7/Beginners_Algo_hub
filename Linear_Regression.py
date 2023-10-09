6320c687c21f9d3743874474a19d5f7609f09c48
thejaswi13 <115213739+thejaswi13@users.noreply.github.com>
2023-10-09 19:49:56 +0530

    Linear_Regression.py
    "Implemented linear regression using the scikit-learn library in Python. This implementation includes data preprocessing, splitting the dataset into training and test sets, training the linear regression model, and making predictions. The model's coefficients and intercept are also printed for analysis and evaluation. This addition enhances the project's capability for predictive modeling."

---
+++ Linear_Regression.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Generate some example data
+np.random.seed(0)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a Linear Regression model
+model = LinearRegression()
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Plot the training data and the linear regression line
+plt.scatter(X_train, y_train, label='Training Data')
+plt.scatter(X_test, y_test, label='Test Data')
+plt.plot(X_test, y_pred, color='red', linewidth=2, label='Linear Regression')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+# Print the model coefficients and intercept
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
